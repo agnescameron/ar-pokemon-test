d940ae283629e9596703c9b276a44f02a46c0525
Agnes Cameron <agnesfcameron@gmail.com>
2022-05-04 16:58:22 +0100

    fixed it

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ window.onload = () => {
     let testEntitiesAdded = false;
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntitiesAdded) {
-                const entity = document.createElement("a-box");
-                entity.setAttribute("scale", {
-                    x: 20, 
-                    y: 20,
-                    z: 20
-                });
-                entity.setAttribute('material', { color: 'yellow' } );
-                entity.setAttribute('gps-new-entity-place', {
-                    latitude: e.detail.position.latitude + 0.001,
-                    longitude: e.detail.position.longitude + 0.001
-                });
-                
-                document.getElementById("scene").appendChild(entity);
-                console.log('added', prop.color)
-            }
+    if(!testEntitiesAdded) {
+            const entity = document.createElement("a-box");
+            entity.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            entity.setAttribute('material', { color: 'yellow' } );
+
+            entity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude + 0.001
+            });
+            
+            document.getElementById("scene").appendChild(entity);
+            console.log('added', prop.color)
             testEntitiesAdded = true;
         }
     });
